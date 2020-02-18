e22ae900ce86692e5c6f663ec62858a9b75cff02
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-02-18 10:51:43 -0800

    Associate rented bikes with customer name key References #2

--- rental_manager.rb
+++ rental_manager.rb
@@ -2,7 +2,13 @@
 
 class RentalManager
 
-    def initialize
+    attr_accessor :customer_bikes
 
+    def initialize()
+        @rented_bikes = Hash.new
+    end
+
+    def add_customer_bikes(customer, bikes)
+        @rented_bikes[customer] = bikes
     end
 end
