8e8db1d584a23d0da5a99b9f3c410fed97295cf8
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-02-18 12:15:57 -0800

    Ask user for bike they are returning References #2

--- return_workflow.rb
+++ return_workflow.rb
@@ -14,13 +14,20 @@ class ReturnWorkflow
 		Bike.new('03', 'Triathlon Bike')])
 	end
 
-	def print_greeting
+	def print_greeting()
 		puts 'Hello ' + @customer.name + ','
 		puts 'You have ' + @rental.number_of_bikes(@customer.name).to_s + ' rented bikes.'
 		puts @rental.print_bikes(@customer.name)
 	end
 
+	def get_returning_bike()
+		puts 'Which bike are you returning? (Please enter the bike ID)'
+		gets.strip.chomp
+	end
+
 	def run()
-		print_greeting
+		print_greeting()
+		get_returning_bike()
+		id = get_returning_bike()
 	end
 end
