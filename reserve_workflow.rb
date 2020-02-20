c83f4411a71bebca167176626c83d4a5aba3d793
Irish Fagan <IrishFagan@live.com>
2020-02-20 11:45:42 -0800

    Add run method. Change naming to match convention References #1

--- reserve_workflow.rb
+++ reserve_workflow.rb
@@ -1,6 +1,5 @@
 require_relative 'customer'
 require_relative 'bike'
-require_relative 'rental'
 require_relative 'inventory_manager'
 
 class ReserveWorkflow
@@ -10,10 +9,13 @@ class ReserveWorkflow
    @rental = Rental.new()
   end
 
-  def Welcome
+  def welcome()
   	puts("Welcome to Bend Bike Shop!")
   	puts("What type of bike would you like to reserve?")
-  	
+  end
+
+  def run()
+  	welcome
   end
 
 end
