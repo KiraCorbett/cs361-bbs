ac04da955f65ae3ac05a98b85bc5f4a2b1fd7efa
Irish Fagan <IrishFagan@live.com>
2020-02-21 12:18:07 -0800

    Fix check_inventory method implementation. Declare inventory as instance variable References #1

--- inventory_manager.rb
+++ inventory_manager.rb
@@ -1,16 +1,20 @@
 class InventoryManager
-	attr_accessor
-	inventory = ["Road","Mountain","Cross","Unicycle","Tricycle", "High Wheel"]
+	attr_accessor :type
 
-	def initialize(type)
-		@type = type
+	def initialize()
+		@inventory = ["Road","Mountain","Cross","Unicycle","Tricycle", "High Wheel"]
 	end
 
-	def check_inventory
-		if inventory.include? 'type'
-			bike.new(01, type)
-		else
-			puts("Not in inventory! :(")
+	def check_inventory(type)
+		#FIX THIS CHECKING OF TYPE IN INVENTORY ARRAY
+		@inventory.each do |i|
+			if i == type
+				Bike.new(rand(1..99), type)
+				break
+			else
+				puts("Not in inventory :(")
+				break
+			end
 		end
 	end
 end
