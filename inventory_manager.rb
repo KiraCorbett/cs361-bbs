a50469c869a395c76d32d03382a07eb44e7ff43f
Irish Fagan <IrishFagan@live.com>
2020-02-23 11:34:56 -0800

    Restructure check_inventory. Reformat to match ruby convention References #1

--- inventory_manager.rb
+++ inventory_manager.rb
@@ -6,29 +6,20 @@ class InventoryManager
 	end
 
 	def check_inventory(inputted_bike_type)
-		#FIXED
     found_type = false
-		@inventory.each do |i|
-			if i == inputted_bike_type
-				puts("excilent choice!")
-				# the line below might be an SRP violation
-				# this isn't really checking inventory it is adding something
-				Bike.new(rand(1..99), inputted_bike_type)
-
-				found_type = true
-        end
-			end
-
-			if found_type == false
-				puts("Not in inventory :(")
-				end
+		if @inventory.include? inputted_bike_type
+      return true
+    else
+      return false
     end
+  end
 
 	def display_all_inventory()
 		@inventory.each { |x| puts x}
-   end
+    puts "..."
+  end
 
-	 def add_to_inventory(x)
+  def add_to_inventory(x)
 		 @inventory << x
-	 end
- end
+  end
+end
