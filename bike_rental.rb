9eda93b6edad23665141b48002806ec5c99df816
kerojosh96 <jaybirdcook@gmail.com>
2020-02-29 17:24:41 -0800

    added more to database. still needs work

--- bike_rental.rb
+++ bike_rental.rb
@@ -1,11 +1,16 @@
 require_relative 'bike_type'
 
 class BikeRental
+ attr_accessor :print_rental
+ def initialize(customer_name,bike_id,date_rented,expected_return)
+   @bike_id = bike_id
+   @customer_name = customer_name
+   @date_rented = date_rented
 
- def initialize(customer_name,date_rented)
-   @customer_name = ""
-   @date_rented = ""
 
+ end
+ def print_rental()
+   print("customer name: ",@customer_name, " | bike ID: ",@bike_id, " Date rented: ",@date_rented)
  end
 
 end
--- bike_rental_database.rb
+++ bike_rental_database.rb
@@ -1,6 +1,10 @@
 
 require_relative 'bike_rental'
 class BikeRentalDatabase
+  attr_accessor :test
+def self.test
+  puts("yo")
+end
 
 def initialize(rentals)
   @rentals = rentals
@@ -8,16 +12,15 @@ end
 
 def print_all_rentals()
   @rentals.each do |num|
-    puts num
-end
-
-
+    puts num.print_rental
 end
 
 end
 
-puts("hello")
+end
 
-a = BikeRentalDatabase.new([BikeRental.new("john doe","12/18/2010"),
-BikeRental.new("jane doe","12/17/2010")])
+
+#for tesing only
+a = BikeRentalDatabase.new([BikeRental.new("john doe","125","12/16/2010","12/18/2010"),
+BikeRental.new("jane doe","125","12/17/2010","12/18/2010")])
 a.print_all_rentals
