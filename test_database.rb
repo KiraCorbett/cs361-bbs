b1ddf8f131e9a4fa465716aa21b72fa97346875b
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-03-02 11:15:02 -0800

    Format code

--- test_database.rb
+++ test_database.rb
@@ -5,8 +5,8 @@ class TestDatabase
 
   def run()
     a = BikeRentalDatabase.new([BikeRental.new("john","125","12/16/2010","12/18/2010",1),
-    BikeRental.new("jane","122","12/17/2010","12/18/2010",2),
-    BikeRental.new("kate","567","12/19/2010","12/20/2010",2)])
+        BikeRental.new("jane","122","12/17/2010","12/18/2010",2),
+        BikeRental.new("kate","567","12/19/2010","12/20/2010",2)])
     a.print_all_rentals
   end
 end
