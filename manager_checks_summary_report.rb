669793d142c7294c76ced4c1ef4e487cbd96e53e
kerojosh96 <jaybirdcook@gmail.com>
2020-03-01 20:21:16 -0800

    new try

--- manager_checks_summary_report.rb
+++ manager_checks_summary_report.rb
@@ -1,7 +1,9 @@
 
 require_relative 'bike_rental_database'
 require_relative 'test_database'
+
 class ManagerChecksRentalDatabase
+puts("summary report information")
 
 b = TestDatabase.new()
 b.run()
