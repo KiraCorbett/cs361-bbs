e4e5dacae466ea03c91faa6dc227122cd58f9559
Kira Corbett <kiracorbett00@gmail.com>
2020-03-06 18:08:24 -0800

    Refactored rental database

--- cancel_workflow.rb
+++ cancel_workflow.rb
@@ -1,16 +1,13 @@
-require_relative 'bike'
-require_relative 'customer'
-require_relative 'rental_manager'
-require_relative 'bike_rental_database'
+require_relative 'rental_database'
 
 class CancellationWorkflow
 
   def initialize()
-    @bike_rental_database = BikeRentalDatabase.new
+    @rental_database = RentalDatabase.new
   end
 
   def get_cancelling_bike()
-    @bike_rental_database.print_all_rentals
+    @rental_database.print_all_rentals
     puts 'Which reservation would you like to cancel?'
     #@rental.select_bike(@customer.name)
   end
