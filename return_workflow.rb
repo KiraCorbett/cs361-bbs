40b3c90055982b64ab0e76e8cd26a6ea2f5caddf
Kira Corbett <kiracorbett00@gmail.com>
2020-03-06 17:56:19 -0800

    Separating cancel and return workflows

--- return_workflow.rb
+++ return_workflow.rb
@@ -7,6 +7,20 @@ class ReturnWorkflow < EndRentalWorkflow
 
   attr_accessor :customer, :rental
 
+  def initialize()
+    @customer = Customer.new('Shirai')
+    @rental = RentalManager.new
+    @rental.add_customer_bikes(@customer.name, [Bike.new('01', 'Mountain Bike'), \
+    Bike.new('02', 'Road Bike'), \
+    Bike.new('03', 'Triathlon Bike')])
+  end
+
+  def print_greeting()
+    puts 'Hello ' + @customer.name + ','
+    puts 'You have ' + @rental.number_of_bikes(@customer.name).to_s + ' rented bikes.'
+    puts @rental.print_bikes(@customer.name)
+  end
+
   def get_returning_bike()
     @rental.select_bike(@customer.name)
   end
