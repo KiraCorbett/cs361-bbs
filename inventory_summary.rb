f4cc35ea6aebb3fcc404dca42b208c1ddb5ece47
Irish Fagan <IrishFagan@live.com>
2020-03-08 22:00:03 -0700

    Change naming to reflect other parts of program. Modify check_rental_type_inventory to accept a rental category and a specified rental type References #8

--- inventory_summary.rb
+++ inventory_summary.rb
@@ -2,27 +2,31 @@ require_relative 'rental_type'
 
 class InventorySummary
   #the old name "inventory" was not descriptive enough
-  attr_accessor :rental_type
+  attr_accessor :rental_category
 
 	def initialize()
 		@bike_inventory = [RentalType.new("Road"), RentalType.new("Mountain"), RentalType.new("Cross"), RentalType.new("Unicycle"), RentalType.new("Tricycle"), RentalType.new("High Wheel")]
     @kayak_inventory = [RentalType.new("Racing"), RentalType.new("Fishing"), RentalType.new("Diving")]
     @inventory = @kayak_inventory + @bike_inventory
-	  @rental_type = ["Bike", "Kayak"]
+	  @rental_category = ["Bike", "Kayak"]
   end
 
   def display_inventory_type()
-    @rental_type.each {|type| puts type }
+    @rental_category.each {|type| puts type }
   end
 
-	def check_rental_type_inventory(inputted_rental_type)
-		return @inventory.any? {|rentalType| rentalType.name = inputted_rental_type}
+	def check_rental_type_inventory(inputted_rental_type, rental_category)
+    if(rental_category == "Kayak")
+		  return @inventory.any? {|rentalType| rentalType.name = inputted_rental_type}
+    elsif (rental_category == "Bike")
+      return @inventory.any? {|rentalType| rentalType.name = inputted_rental_type}
+    end
   end
 
-	def display_rental_type_inventory(type)
-		if type == "Bike"
+	def display_rental_type_inventory(rental_category)
+		if rental_category == "Bike"
       @bike_inventory.each {|rentalType| puts rentalType.name}
-    elsif type == "Kayak"
+    elsif rental_category == "Kayak"
       @kayak_inventory.each {|rentalType| puts rentalType.name}
     end
     puts "..."
