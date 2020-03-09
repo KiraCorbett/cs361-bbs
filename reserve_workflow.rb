5cdb0bb32136e56e663c188915c0eab3ba438c19
Irish Fagan <IrishFagan@live.com>
2020-03-08 21:31:42 -0700

    Add get_rental_type method. Rename call in display_inventory_with_message method References #8

--- reserve_workflow.rb
+++ reserve_workflow.rb
@@ -12,9 +12,18 @@ class ReserveWorkflow
   	puts "Welcome to Bend Bike Shop!"
   end
 
-  def display_inventory_with_message
+  def get_rental_type()
+    puts "Here's a selection of the rentals we currently have!"
+    @rental.display_inventory_type
+    inputted_rental_type = gets.strip.chomp 
+    if @rental.rental_type.include? inputted_rental_type
+      display_inventory_with_message(inputted_rental_type)
+    end
+  end
+
+  def display_inventory_with_message(rental_type)
    puts "Our inventory includes the following: "
-   @rental.display_all_rental_type_inventory
+   @rental.display_rental_type_inventory(rental_type)
   end
 
   def get_bike_type()
@@ -57,8 +66,6 @@ end
 
   def run()
   	welcome
-    display_inventory_with_message
-    get_bike_type
-    get_reservation_request
+    get_rental_type
   end
 end
